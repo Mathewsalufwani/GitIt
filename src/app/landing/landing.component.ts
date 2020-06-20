dd41c6316ea179b001a0ad0fc77ea32cbcc31ac6
Mathews Alufwani <mathewalufwani@gmail.com>
2020-06-20 21:37:54 +0300

    visual improvements

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -17,11 +17,16 @@ export class LandingComponent implements OnInit {
     interface ApiResponse{
       login:string;
       bio:string;
+      name:string;
+      avatar_url:string;
+      followers:number;
+      following:number;
+      created_at:Date
     }
 
-    this.http.get<ApiResponse>("https://api.github.com/users/Mathewsalufwani?Authorization: token f03ef7af6eb4fa6a7e2fda8137b60ab8f165bfa2").subscribe(data=>{
+    this.http.get<ApiResponse>("https://api.github.com/users/Mathewsalufwani?Authorization: token d1b35c03b5f715d346ef08c8212b7875fa2f2c1e").subscribe(data=>{
       // Succesful API request
-      this.user = new User(data.login, data.bio)
+      this.user = new User(data.login, data.bio, data.name, data.avatar_url, data.followers, data.following, data.created_at)
     })
   }
 
--- src/app/user.ts
+++ src/app/user.ts
@@ -1,3 +1,3 @@
 export class User {
-  constructor(public login:string, public bio:string ){}
+  constructor(public login:string, public bio:string, public name:string, public avatar_url:string, public followers:number, public following:number, public created_at:Date){}
 }
