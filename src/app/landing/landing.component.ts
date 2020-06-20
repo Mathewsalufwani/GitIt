ca5d556554ddae24741ae9c97ff8943787d1f3f6
Mathews Alufwani <mathewalufwani@gmail.com>
2020-06-20 19:05:46 +0300

    add user class

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../user';
 
 @Component({
   selector: 'app-landing',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
-  constructor() { }
+  user:User;
 
-  ngOnInit(): void {
+  constructor( private http:HttpClient ) { }
+
+  ngOnInit() {
+    interface ApiResponse{
+      login:string;
+      bio:string;
+    }
+
+    this.http.get<ApiResponse>("https://api.github.com/users/Mathewsalufwani?Authorization: token f03ef7af6eb4fa6a7e2fda8137b60ab8f165bfa2").subscribe(data=>{
+      // Succesful API request
+      this.user = new User(data.login, data.bio)
+    })
   }
 
 }
