9031b250d3dee865894b6a5132295d55f6ce2434
Mathews Alufwani <mathewalufwani@gmail.com>
2020-06-21 16:42:38 +0300

    beggin routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+import { SearchUserComponent } from './search-user/search-user.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'landing', component: LandingComponent},
+  { path: 'search', component: SearchUserComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
