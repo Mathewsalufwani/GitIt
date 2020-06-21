73d2bf5cffc95ec6dabbabbabc8b3fb31cabd463
Mathews Alufwani <mathewalufwani@gmail.com>
2020-06-21 14:38:11 +0300

    add directive logic

--- src/app/on-hover.directive.ts
+++ src/app/on-hover.directive.ts
@@ -1,10 +1,22 @@
-import { Directive } from '@angular/core';
+import { Directive,  ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appOnHover]'
 })
 export class OnHoverDirective {
 
-  constructor() { }
+  constructor(private elem:ElementRef) {}
+
+  @HostListener("mouseover") onMouseOver(){
+    this.text("#8080ff")
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.text("black");
+  }
+
+  private text(action:string){ 
+    this.elem.nativeElement.style.color=action;
+  }
 
 }
