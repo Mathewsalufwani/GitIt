850978419c852d56ec17b822ab6f1988371840cd
Mathews Alufwani <mathewalufwani@gmail.com>
2020-06-21 13:41:58 +0300

    minor visual adjustments

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../user';
+import { Repo } from '../repo';
 
 @Component({
   selector: 'app-landing',
@@ -10,6 +11,7 @@ import { User } from '../user';
 export class LandingComponent implements OnInit {
 
   user:User;
+  repo:Repo;
 
   constructor( private http:HttpClient ) { }
 
@@ -23,11 +25,21 @@ export class LandingComponent implements OnInit {
       following:number;
       created_at:Date;
       public_repos:number;
+      
     }
 
-    this.http.get<ApiResponse>("https://api.github.com/users/Mathewsalufwani?Authorization: token d1b35c03b5f715d346ef08c8212b7875fa2f2c1e").subscribe(data=>{
+    interface repoApiResponse{
+      full_name:string;
+    }
+
+    this.http.get<ApiResponse>("https://api.github.com/users/Mathewsalufwani?Authorization: token 68aba20afd5a1fa2e4b541cefb496075806e2507").subscribe(data=>{
       // Succesful API request
       this.user = new User(data.login, data.bio, data.name, data.avatar_url, data.followers, data.following, data.created_at, data.public_repos)
+    })
+
+    this.http.get<repoApiResponse>("https://api.github.com/users/Mathewsalufwani/repos?client_id=68aba20afd5a1fa2e4b541cefb496075806e2507").subscribe(data=>{
+      // Succesful API request
+      this.repo = new Repo(data.full_name)
     })
   }
 
