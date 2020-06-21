2cb4611578544a672754d788c12bcaaab78520b6
Mathews Alufwani <mathewalufwani@gmail.com>
2020-06-21 17:45:49 +0300

    add not found logic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
 import { SearchUserComponent } from './search-user/search-user.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 
@@ -9,6 +10,7 @@ const routes: Routes = [
   { path: 'landing', component: LandingComponent},
   { path: 'search', component: SearchUserComponent},
   { path: '', redirectTo:"/landing", pathMatch:"full"},
+  { path:'**', component:NotFoundComponent},
   
 ];
 
